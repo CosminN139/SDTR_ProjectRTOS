fd174e04c66f383e1fb78dcf38fbd41ee84c95d4
Cos <c.nastase@outlook.com>
2020-10-15 14:11:09 +0300

    added FreeRTOS dependencies and first project startups

---
+++ MyProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * MyProject.c
+ *
+ * Created: 10/15/20 1:39:46 PM
+ * Author : cnast
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
